9563df950d058680c5e402a790f5bc7691eec0d8
Alex Nowak <alex.nowak@gmx.de>
2011-02-01 14:38:48 -0500

    message

---
+++ app/models/player.rb
@@ -0,0 +1,21 @@
+class Player < ActiveRecord::Base
+
+  validates :username, :email, :firstname, :lastname, :presence => true,  :length => {:minimum => 2}
+  validate :valid_email?
+  validates_uniqueness_of :username, :email
+
+  before_validation do
+    puts "email=\'#{email}\'"
+    self.email = self.email.strip
+  end
+
+  # Validate email syntax.
+  # For now, an email is accepted if it has one @ and no special chars
+  def valid_email?
+    puts "email: '#{email}' split= #{email.split(/@/).size}"
+    errors.add(:email,"Must be a valid email") if
+         email.split(/@/).size != 2 || email.index(/[\s\!\#\$\%\^\&\*\(\)\+\=\~\`\"\<\>\[\]\:\;\?\/\\\|\{\}]/) != nil
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tennisbox::Application.config.secret_token = 'af8ba1c04e2e879ea028825ab0af967e82d727b3bdfbd367f7583106b673f6a18f6d254ce059bc1b081e38c4bf5fc2cb64f77548f3808a23e706841f1678867f'
